dceb926c1dd8cfffea816dcdfe28a809ce7cfd70
Mike DelGaudio <delgaudiomike@gmail.com>
2021-04-23 20:58:03 -0400

    fix env; add breakpoints; add navbar; start store

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-
+import './Navbar.scss';
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <>{/* <header className="neu-header">
+    return (
+      <>
+        <header className="neu-header">
           <h1>Neu</h1>
-        </header> */}</>;
+        </header>
+      </>
+    );
   }
 }
 
