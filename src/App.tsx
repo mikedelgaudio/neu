e86401de38ac3321607d24690f9cfdd33c4b66df
Mike DelGaudio <mdelgaud@stevens.edu>
2021-06-21 19:43:17 -0400

    switch to func component

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,14 @@ import './App.scss';
 import Footer from './components/footer/Footer';
 import Main from './components/main/Main';
 import Navbar from './components/navbar/Navbar';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <>
-        <div className="neu-frame">
-          <Navbar />
-          <Main />
-          <Footer />
-        </div>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="neu-frame">
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
