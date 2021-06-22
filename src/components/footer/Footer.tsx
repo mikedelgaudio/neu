39bf8739ad937069c628c55d6474c577b26d2945
Mike DelGaudio <mdelgaud@stevens.edu>
2021-06-21 21:20:15 -0400

    delete extra colors sheet

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,7 @@
-import { Component } from 'react';
 import './Footer.scss';
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <>
-        <div className="add"></div>
-      </>
-    );
-  }
-}
+const Footer = () => {
+  return <div className="add"></div>;
+};
 
 export default Footer;
